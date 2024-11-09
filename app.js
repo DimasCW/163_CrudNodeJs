7a858f9ef8ac9c9f3ab4b69b5c134d1461df1f65
DimasCW <cdimas292@gmail.com>
2024-11-09 14:08:54 +0700

    dbcrud_163

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-const todoRoutes = require ('./routes/todo.js');
-const port = 3000;
+const todoRoutes = require ('./routes/tododb.js');
+require('dotenv').config();
+const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
+
 
 app.use(express.json());
 
@@ -9,13 +14,28 @@ app.use('/todos', todoRoutes);
 
 app.set('view engine' , 'ejs');
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout : 'layouts/main-layout'
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout : 'layouts/main-layout'
+    });
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
