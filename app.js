3ab534128eb389a8481b89ef480520c599bf5c12
DimasCW <cdimas292@gmail.com>
2024-11-22 20:31:44 +0700

    Midleware

--- app.js
+++ app.js
@@ -6,26 +6,39 @@ const port = process.env.PORT;
 const db = require('./database/db');
 const expressLayouts = require('express-ejs-layouts')
 app.use(expressLayouts);
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
 
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET, // Gunakan secret key yang aman
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
+}));
+
+app.use('/', authRoutes);
 app.use('/todos', todoRoutes);
 
 app.set('view engine' , 'ejs');
-app.get('/', (req, res) => {
+app.get('/', isAuthenticated,(req, res) => {
     res.render('index', {
         layout : 'layouts/main-layout'
     });
 });
 
-app.get('/contact', (req, res) => {
+app.get('/contact', isAuthenticated,(req, res) => {
     res.render('contact', {
         layout : 'layouts/main-layout'
     });
 });
 
-app.get('/todo-view', (req, res) => {
+app.get('/todo-view',isAuthenticated, (req, res) => {
     db.query('SELECT * FROM todos', (err, todos) => {
         if (err) return res.status(500).send('Internal Server Error');
         res.render('todo', {
